2cacbf8bce3cadc0992cc5cd15a0971075041657
Claus Stadler <RavenArkadon@googlemail.com>
2023-02-26 09:02:38 +0100

    Fixed build and updated dataset

--- iana-language-subtag-registry-rdfizer/src/main/java/org/aksw/iana/language/subtag/registry/rdfizer/MainIanaSubTagRegistryExporter.java
+++ iana-language-subtag-registry-rdfizer/src/main/java/org/aksw/iana/language/subtag/registry/rdfizer/MainIanaSubTagRegistryExporter.java
@@ -1,7 +1,8 @@
 package org.aksw.iana.language.subtag.registry.rdfizer;
 
 import org.aksw.iana.subtag.registry.domain.IanaSubTag;
-import org.aksw.jena_sparql_api.mapper.proxy.JenaPluginUtils;
+import org.aksw.jenax.reprogen.core.JenaPluginUtils;
+import org.aksw.jenax.sparql.query.rx.RDFDataMgrEx;
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.riot.RDFDataMgr;
 import org.apache.jena.riot.RDFFormat;
@@ -9,15 +10,16 @@ import org.apache.jena.riot.RDFFormat;
 public class MainIanaSubTagRegistryExporter {
 
 
-	public static void main(String[] args) throws Exception {
-		JenaPluginUtils.registerResourceClasses(IanaSubTag.class);
-		
+    public static void main(String[] args) throws Exception {
+        JenaPluginUtils.registerResourceClasses(IanaSubTag.class);
 
-		//URL url = new URL("http://www.iana.org/assignments/language-subtag-registry/language-subtag-registry");
+        //URL url = new URL("http://www.iana.org/assignments/language-subtag-registry/language-subtag-registry");
 
-		Model model = IanaLanguageSubtagRegistryRdfizer.rdfize("http://www.iana.org/assignments/language-subtag-registry/language-subtag-registry");
-		
-		RDFDataMgr.write(System.out, model, RDFFormat.TURTLE_PRETTY);
-	}
+        Model model = IanaLanguageSubtagRegistryRdfizer.rdfize("http://www.iana.org/assignments/language-subtag-registry/language-subtag-registry");
+
+        Model validated = RDFDataMgrEx.printParseRoundtrip(model, RDFFormat.TURTLE, null);
+
+        RDFDataMgr.write(System.out, validated, RDFFormat.TURTLE_PRETTY);
+    }
 
 }
--- iana-language-subtag-registry-rdfizer/src/main/java/org/aksw/iana/subtag/registry/domain/IanaSubTag.java
+++ iana-language-subtag-registry-rdfizer/src/main/java/org/aksw/iana/subtag/registry/domain/IanaSubTag.java
@@ -1,18 +1,48 @@
 package org.aksw.iana.subtag.registry.domain;
 
-import org.aksw.jena_sparql_api.mapper.annotation.Iri;
-import org.aksw.jena_sparql_api.mapper.annotation.ResourceView;
+import org.aksw.jenax.annotation.reprogen.Iri;
+import org.aksw.jenax.annotation.reprogen.ResourceView;
 import org.apache.jena.rdf.model.Resource;
 
 @ResourceView
 public interface IanaSubTag
-	extends Resource
+    extends Resource
 {
-	@Iri("urn:Type")
-	String getType();
-	IanaSubTag setType(String type);
-	
-	@Iri("urn:Subtag")
-	String getSubtag();
-	IanaSubTag setSubtag(String subtag);
+    // TODO Change String to LocalDate
+    @Iri("urn:x-key:Added")
+    String getAdded();
+    IanaSubTag setAdded(String added);
+
+    // TODO Change String to LocalDate
+    @Iri("urn:x-key:Comments")
+    String setComments();
+    IanaSubTag getComments(String comments);
+
+    @Iri("urn:x-key:Deprecated")
+    String getDeprecated();
+    IanaSubTag setDeprecated(String deprecated);
+
+    @Iri("urn:x-key:Description")
+    String getDescription();
+    IanaSubTag setDescription(String description);
+
+    @Iri("urn:x-key:Subtag")
+    String getSubtag();
+    IanaSubTag setSubtag(String subtag);
+
+    @Iri("urn:x-key:Type")
+    String getType();
+    IanaSubTag setType(String type);
+
+    @Iri("urn:x-key:Preferred-Value")
+    String getPreferredValue();
+    IanaSubTag setPreferredValue(String preferredValue);
+
+    @Iri("urn:x-key:Prefix")
+    String getPrefix();
+    IanaSubTag setPrefix(String prefix);
+
+    @Iri("urn:x-key:MacroLanguage")
+    String getMacroLanguage();
+    IanaSubTag setMacroLanguage(String macroLanguage);
 }
